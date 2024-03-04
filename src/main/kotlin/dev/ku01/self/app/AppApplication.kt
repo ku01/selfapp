236e0985ef6f5dff4266a7d9a449f7c64813c4c2
Ivan Kupriyanov <i.a.kuprianov@gmail.com>
2024-03-04 14:03:15 +0100

    Initial commit

---
+++ src/main/kotlin/dev/ku01/self/app/AppApplication.kt
@@ -0,0 +1,11 @@
+package dev.ku01.self.app
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AppApplication
+
+fun main(args: Array<String>) {
+	runApplication<AppApplication>(*args)
+}

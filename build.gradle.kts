e3a0b46684c56a80c547658ababcd9bb1beac121
Ivan Kupriyanov <i.a.kuprianov@gmail.com>
2024-03-04 14:30:48 +0100

    Add actuator

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,9 @@ dependencies {
 
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+	implementation("io.micrometer:micrometer-registry-prometheus")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
